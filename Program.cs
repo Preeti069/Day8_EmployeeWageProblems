96d44707f9ad4ba154d547f86abb3e6e12e6ceea
preeti <pritich0896@gmail.com>
2023-01-01 13:44:40 +0530

    Welcome Message

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Part3_EmployeeWagePro
+{
+    public class Program
+    {
+                
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Employee Wage Program\n");
+            EmployeeCheck Employee = new EmployeeCheck();
+          Employee.display();
+
+           Console.ReadLine();
+        }
+        
+    }
+}
+    

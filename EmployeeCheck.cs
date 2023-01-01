7f23dfd487ba237868b0b3514bb20c992f4d8615
preeti <pritich0896@gmail.com>
2023-01-01 14:19:59 +0530

    Add Part time Employee & Wage

--- EmployeeCheck.cs
+++ EmployeeCheck.cs
@@ -11,20 +11,26 @@ namespace Part3_EmployeeWagePro
         public void display()
         {
             int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
             int EMP_RATE_PER_HR = 20;
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-            int empcheck = random.Next(0, 2);
+            int empcheck = random.Next(0, 3);
             if (empcheck == IS_FULL_TIME)
             {
                 empHrs = 8;
                 Console.WriteLine("Employee is Present");
             }
+            else if (empcheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+                Console.WriteLine("EMployee is Part Time");
+            }
             else
             {
                 empHrs = 0;
-                Console.WriteLine("Employee  is Absent");
+                Console.WriteLine("Employee is Absent");
             }
             empWage = EMP_RATE_PER_HR * empHrs;
             Console.WriteLine("Employee Daily Wage is : " + empWage);

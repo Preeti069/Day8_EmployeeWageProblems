74969c30913d309097bf5688ddc9ca6c1f22cb07
preeti <pritich0896@gmail.com>
2023-01-01 14:45:45 +0530

    days is reached fora month

--- EmployeeCheck.cs
+++ EmployeeCheck.cs
@@ -8,15 +8,18 @@ namespace Part3_EmployeeWagePro
 {
     public class EmployeeCheck
     {
+
         public void display()
         {
             int IS_FULL_TIME = 1;
             int IS_PART_TIME = 2;
             int EMP_RATE_PER_HR = 20;
             int NUM_OF_WORKING_DAYS = 20;
-            int empHrs = 0, empWage = 0, totalEmpwage = 0;
-            for (int day = 0; NUM_OF_WORKING_DAYS > day; day++)
+            int MAX_HRS_IN_MONTH = 100;
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
+                totalWorkingDays++;
                 Random random = new Random();
                 int empcheck = random.Next(0, 3);
                 switch (empcheck)
@@ -36,11 +39,11 @@ namespace Part3_EmployeeWagePro
                         Console.WriteLine("Employee is Absent");
                         break;
                 }
-                empWage = EMP_RATE_PER_HR * empHrs;
-                totalEmpwage += empWage;
-                Console.WriteLine("Employee Daily Wage is : " + empWage);
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day : " + totalWorkingDays + "Emp HRS : " + empHrs);
             }
-            Console.WriteLine("Employee Wage For 20 Days Of Month is :" + totalEmpwage);
+            int totalEmpWage = totalEmpHrs + EMP_RATE_PER_HR;
+            Console.WriteLine("Employee Wage For 20 Days Of Month is :" + totalEmpWage);
         }
     }
 }
